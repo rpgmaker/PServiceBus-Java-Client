bed87db4beea3815d57eca767020038c5914eac7
rpgmaker <olamide.bakre@gmail.com>
2013-02-03 01:21:51 -0500

    Added preference logic for Java localstorage updated jars to latest code output
    Signed-off-by: rpgmaker <olamide.bakre@gmail.com>

--- src/client/src/psb/JavaLocalStorage.java
+++ src/client/src/psb/JavaLocalStorage.java
@@ -1,20 +1,38 @@
 package psb;
 
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
 class JavaLocalStorage implements ILocalStorage {
 
+	private Preferences pref;
+	
+	public JavaLocalStorage(){
+		pref = Preferences.userRoot();
+		pref = pref.node(System.getProperty("user.dir"));
+	}
+	
+	private void flush(){
+		try {
+			pref.flush();
+		} catch (BackingStoreException e) {}
+	}
+	
 	@Override
 	public void set(String key, String value) {
-		
+		pref.put(key, value);
+		flush();
 	}
 
 	@Override
 	public String get(String key) {
-		return null;
+		return pref.get(key, null);
 	}
 	
 	@Override
 	public void remove(String key){
-		
+		pref.remove(key);
+		flush();
 	}
 
 	@Override
